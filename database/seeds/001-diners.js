777e48b7a1e1c6ea34a7a0fccb9146ebf2a366b5
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-11-18 13:54:16 -0600

    seeds

--- database/seeds/001-diners.js
+++ database/seeds/001-diners.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('diners').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('diners').insert([
+        {diner_name: 'jerry smith' , location: 'Austin, TX'},
+        {diner_name: 'rick sanchez' , location: 'Carrollton, TX'},
+        {diner_name: 'jeff Jimson' , location: 'Rockford, IL'},
+        {diner_name: 'Justin Nichols' , location: 'St. Paul, MN'},
+        {diner_name: 'Yohana Torress' , location: 'Sicklerville, NJ'},
+        {diner_name: 'tim bobinsons' , location: 'Bee Cave, TX'}
       ]);
     });
 };
--- database/seeds/002-operators.js
+++ database/seeds/002-operators.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('diners').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('diners').insert([
+        {company_name: 'good eats'},
+        {company_name: 'bobs burger trucks'},
+        {company_name: 'mobile food solutions'},
+        {company_name: 'grub n go'},
+        {company_name: 'food fast llc'},
+        {company_name: 'i own trucks'}
       ]);
     });
 };
