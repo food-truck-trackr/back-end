eebe45cb3528bcaeed2d063bb19f8fff97dbae00
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-11-20 14:29:51 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -18,5 +18,32 @@ module.exports = {
     seeds: {
       directory: './database/seeds'
     }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   }
-};
+}[ process.env.NODE_ENV || 'development' ];
