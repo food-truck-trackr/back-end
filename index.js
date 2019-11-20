a1f1463bc58ab60e5da5d66832401a00affad76b
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-11-20 15:03:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const server = require('./api/server.js');
+
+const dbPort = process.env.DB_PORT;
+
 require('dotenv').config();
-const server = require('./api/server');
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || dbPort || 4000;
 
-server.listen(port, () => {
-    console.log(`IM HERE ON PORT ${port}`);
-}); 
+server.listen(PORT, () => {
+    console.log(`!!! Server is listening on port ${PORT} !!!`)
+})
