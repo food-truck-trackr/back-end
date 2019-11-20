9029a2175ade128eb6cfe7a236352079e8661e83
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-11-20 15:23:15 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,25 +1,22 @@
-const knex = require('knex')('development')
 const express = require('express');
+const server = express();
 const cors = require('cors');
 const helmet = require('helmet');
-
-
-
-const usersRouter = require('./users/user-router.js');
-const dinerRouter = require('./diners/diners-router');
+const usersRouter = require('./users/user-router');
+// const dinerRouter = require('./diners/diners-router');
 const trucksRouter = require('./trucks/trucks-router');
 const operatorsRouter = require('./operators/operator-router');
-// const restricted = require('../api/middleware/restricted-middleware');
+const restricted = require('../api/middleware/restricted-middleware');
 
-const server = express();
+
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 server.use('/api', usersRouter)
-server.use('/api/',  dinerRouter)
+// server.use('/api/diners', restricted, dinerRouter)
 server.use('/api/trucks', trucksRouter)
-server.use('/api/operators',operatorsRouter)
+server.use('/api/operators', restricted, operatorsRouter)
 // server.use('/api/docs', express.static('./docs'))
 
 
