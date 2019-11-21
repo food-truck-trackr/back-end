914a0c08f9452de9afddac7f55b12a1287a0abdd
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-11-20 18:08:11 -0600

    /

--- knexfile.js
+++ knexfile.js
@@ -40,7 +40,8 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './database/migrations'
+      directory: './database/migrations',
+      tableName: 'knex_migrations'
     },
     seeds: {
       directory: './database/seeds'
